d59d65f3fdcaf8e5d2cebbb8542443c26d68d196
Mahesh <maheshh.kumar1508@gmail.com>
2026-04-05 21:39:09 +0530

    displaying previous and next image

--- src/pages/ImageSlider.jsx
+++ src/pages/ImageSlider.jsx
@@ -11,23 +11,7 @@ function ImageSlider() {
     ]
     return (
         <div className='w-[90%] mx-auto bg-gray-800 overflow-hidden flex border-4 border-red-500'>
-            <div className="flex items-center justify-center gap-[1em] group">
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">1</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">2</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">3</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">4</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">5</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">6</div>
-            </div>
-            <div className="flex items-center justify-center gap-[1em] group">
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">1</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">2</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">3</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">4</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">5</div>
-                <div className="card h-[5em] p-[1em] grow-0 shrink-0 bg-blue-300 content-center">6</div>
-            </div>
-            {/* <ImageCarousel images={images} /> */}
+            <ImageCarousel images={images} />
         </div>
     )
 }
