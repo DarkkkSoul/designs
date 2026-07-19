d76df2b612a7e4cfd53001b7f11ee400e410e350
Mahesh <maheshh.kumar1508@gmail.com>
2026-07-20 00:11:05 +0530

    little hover animation for stack

--- src/components/icons/Stack.jsx
+++ src/components/icons/Stack.jsx
@@ -1,19 +1,72 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React, { useRef } from 'react'
 
 function Stack() {
+    const stackRef = useRef(null)
+    const topRef = useRef(null)
+    const leftRef = useRef(null)
+    const frontRef = useRef(null)
+
+    gsap.registerPlugin(useGSAP)
+    const { contextSafe } = useGSAP({ scope: stackRef })
+
+    const handleEnter = contextSafe(() => {
+        gsap.to(topRef.current, {
+            duration: 0.2,
+            z: 48-2,
+            ease:"power1.inOut"
+        })
+        gsap.to(leftRef.current, {
+            duration: 0.2,
+            y: 196-2,
+            ease:"power1.inOut"
+        })
+        gsap.to(frontRef.current, {
+            duration: 0.2,
+            x: 128-2,
+            ease:"power1.inOut"
+        })
+    })
+
+    const handleLeave = contextSafe(() => {
+        gsap.to(topRef.current, {
+            duration: 0.2,
+            z: 20,
+            ease:"power1.inOut"
+        })
+        gsap.to(leftRef.current, {
+            duration: 0.2,
+            y: 180,
+            ease:"power1.inOut"
+        })
+        gsap.to(frontRef.current, {
+            duration: 0.2,
+            x: 100,
+            ease:"power1.inOut"
+        })
+    })
+
     return (
-        <div className='flex items-center justify-center perspective-[1000px] relative row-span-3 col-span-2 bg-[#282828]'>
-            <div className='bg-purple-300 size-50 blur-2xl rounded-full absolute' />
-            <div className='relative transform-3d w-50 h-50 rotate-z-43 rotate-x-65'>
-                {/* top & btm  */}
-                <div className='absolute w-50 h-50 bg-linear-to-r from-white to-purple-500 translate-z-10 ' />
-                <div className='absolute w-50 h-50 bg-green-300 -translate-z-10' />
+        <div className='flex items-center justify-center perspective-[1000px] relative row-span-3 col-span-2'
+            ref={stackRef}
+        >
+            <div className='size-50 blur-2xl rounded-full absolute' />
+            <div
+                onMouseEnter={handleEnter}
+                onMouseLeave={handleLeave}
+                className='relative transform-3d w-50 h-50 rotate-z-43 rotate-x-65'>
+                {/* top */}
+                <div className='absolute w-50 h-50 translate-z-5 bg-[#f2f2f2] uppercase text-6xl text-center flex items-center justify-center font-extralight' ref={topRef} >Hover Me</div>
+                <div className='absolute w-50 h-50 -translate-z-5 border border-white border-dashed' />
 
-                 {/* left right    */}
-                <div className='absolute w-50 h-20 bg-blue-300 -translate-y-10 rotate-x-90' />
-                <div className='absolute w-50 h-20 bg-linear-to-r from-purple-200 to-purple-800 translate-y-40 rotate-x-90' />
+                {/* left */}
+                <div className='absolute w-50 h-10 -translate-y-5 rotate-x-90 border border-white border-dashed' />
+                <div className='absolute w-50 h-10 translate-y-45 rotate-x-90 bg-[#f2f2f2]' ref={leftRef} />
 
-                <div className='absolute w-50 h-20 bg-linear-to-b to-purple-800 from-purple-400 translate-x-25 translate-y-15 -rotate-x-90 rotate-y-90' />
+                {/* front */}
+                <div className='absolute w-50 h-10 translate-x-25 translate-y-20 -rotate-x-90 rotate-y-90 bg-[#f2f2f2]' ref={frontRef} />
+                <div className='absolute w-50 h-10 -translate-x-25 translate-y-20 -rotate-x-90 rotate-y-90 border border-white border-dashed' />
 
             </div>
         </div>
